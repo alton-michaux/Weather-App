495db12cc6a87984ea647f81fb3a10366a009281
Alton Michaux <alton.michaux@yahoo.com>
2021-01-22 18:08:25 -0500

    preparing project ,but got caught up trying log the value of the city variable to the console. I need to fix this before I move forward

--- weather.js
+++ weather.js
@@ -1,1 +1,15 @@
+const apiKey = 'a12fb02034883e480f0f41431feb3261';
+const city = document.getElementById('city').value;
+const search = document.getElementById('search');
 
+async function getJSON () {
+    try {
+
+    } catch {
+
+    }
+}
+
+search.addEventListener('click', () => {
+    console.log(city);
+})
