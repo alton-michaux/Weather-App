56cf1448ea2523e28d21a6782281369d1f1bb6af
Alton Michaux <alton.michaux@yahoo.com>
2021-01-22 22:38:17 -0500

    merge commit

--- weather.js
+++ weather.js
@@ -1,14 +1,14 @@
 const apiKey = 'a12fb02034883e480f0f41431feb3261';
-const city = document.getElementById('city').value;
+const city = document.getElementById('city').textContent;
 const search = document.getElementById('search');
 
-async function getJSON () {
-    try {
+// async function getJSON () {
+//     try {
 
-    } catch {
+//     } catch {
 
-    }
-}
+//     }
+// }
 
 search.addEventListener('click', () => {
     console.log(city);
