c3ff7011d1f664117325b0b7fb966db55b0bcebb
Alton Michaux <michauxboyz@yahoo.com>
2021-01-23 02:03:54 +0000

    throwing an error that includes the repl.it address in front of fetch url. Will push and pull in vs code to see if that doesn't fix the problem.

--- weather.js
+++ weather.js
@@ -1,27 +1,32 @@
 const apiKey = 'a12fb02034883e480f0f41431feb3261';
 
-async function getJSON (url) {
-    try {
-      let data = await fetch (url);
-      return await data.json()
-    } catch (err) {
-      throw err;
-    }
+async function getJSON(url) {
+  try {
+    let data = await fetch(url);
+    console.log(data)
+    return await data.json()
+  } catch (err) {
+    throw err;
+  }
 }
 
 const search = document.getElementById('search')
-    
+
 search.addEventListener('click', () => {
-    const cityField = document.getElementById('city').value.toLowerCase();
+  const cityField = document.getElementById('city').value.toLowerCase();
+  if (cityField.length == '0') {
+    alert('Please enter a city')
+  } else {
     const cityURL = `api.openweathermap.org/data/2.5/weather?q=${cityField}&appid=${apiKey}`
     getCity(cityURL)
-      // .then()
-      // .catch()
-    // console.log(cityURL)
+    // .then(data => console.log(data))
+    // .catch(err => console.log(err))
+  }
 })
 
-async function getCity (url) {
+async function getCity(url) {
   const currentCity = await getJSON(url);
   console.log(currentCity)
-  // const locationURL = `https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=${apiKey}`
+  const locationURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${currentCity.coord.lat}&lon=${currentCity.coord.lon}&exclude={part}&appid=${apiKey}`
+  console.log(locationURL)
 }
