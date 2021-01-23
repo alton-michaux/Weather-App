310f90384fb1486af85af92de5a3bfde570989ce
Alton Michaux <michauxboyz@yahoo.com>
2021-01-23 01:01:56 +0000

    finally figured out console.log issue and created fetch function as well as started building out the event listener for the search button

--- weather.js
+++ weather.js
@@ -1,15 +1,27 @@
 const apiKey = 'a12fb02034883e480f0f41431feb3261';
-const city = document.getElementById('city').value;
-const search = document.getElementById('search');
 
-async function getJSON () {
+async function getJSON (url) {
     try {
-
-    } catch {
-
+      let data = await fetch (url);
+      return await data.json()
+    } catch (err) {
+      throw err;
     }
 }
 
+const search = document.getElementById('search')
+    
 search.addEventListener('click', () => {
-    console.log(city);
+    const cityField = document.getElementById('city').value.toLowerCase();
+    const cityURL = `api.openweathermap.org/data/2.5/weather?q=${cityField}&appid=${apiKey}`
+    getCity(cityURL)
+      // .then()
+      // .catch()
+    // console.log(cityURL)
 })
+
+async function getCity (url) {
+  const currentCity = await getJSON(url);
+  console.log(currentCity)
+  // const locationURL = `https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=${apiKey}`
+}
