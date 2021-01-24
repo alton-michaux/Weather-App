0b6056c9ce6289850d199f0fdb457832f2b93bef
Alton Michaux <alton.michaux@yahoo.com>
2021-01-24 13:58:55 -0500

    temperature switch button is now functional, user is now able to toggle between celsius and farenheit readings

--- weather.js
+++ weather.js
@@ -14,48 +14,37 @@ async function getJSON(url) {
 //function that selects the search button.
 const search = document
   .getElementById("search")
-  //makes search button functional 'click'
+  //makes search button functional with 'click'
   .addEventListener("click", () => {
-    //processes user input
+    //process user input from 'cityField'
     const cityField = document.getElementById("city").value.toLowerCase();
     if (cityField.length == "0") {
       alert("Please enter a city");
     } else {
       //generates input-specific url
       const cityURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityField}&appid=${apiKey}`;
+      //refer to functions for relevant comments
       const newData = getCity(cityURL)
-        .then(displayData)
+        .then(displayDataFarenheit)
         .then(tempChangeButton)
         .catch(notFound);
       return newData;
     }
   });
 
-//function that runs previously generated url through the getJSON function for retrieval and parsing as well as generating a second api call for future weather
+//function that runs previously generated url through the getJSON function for retrieval and parsing as well as generating a second api call for future weather forecasts
 async function getCity(url) {
   const currentCity = await getJSON(url);
   const locationURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${currentCity.coord.lat}&lon=${currentCity.coord.lon}&exclude={part}&appid=${apiKey}`;
   const cityInfo = await getJSON(locationURL);
+
   return Promise.all([currentCity, cityInfo]);
 }
 
-//displays data in html format for user to read information
-function displayData(data) {
-  console.log(data);
+//displays data for user to read information (degrees in farenheit)
+function displayDataFarenheit(data) {
   const info = document.getElementById("info");
-  //create a variable for date and format it to appear on page using options variable
-  const date = new Date(data[0].dt * 1000);
-  const options = {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  };
-  //write html to page
-  let newHTML = `<div><h4 class="date">${date.toLocaleDateString(
-    "en-US",
-    options
-  )}</h4>`;
+  let newHTML = getDate(data);
   newHTML += `<p>City/Country: <b>${data[0].name}</b> ,<b>${data[0].sys.country}</b></p>`;
   newHTML += `<p>Current Weather: <b>${data[0].weather[0].description}</b> </p>`;
   newHTML += `<p class='temp'>Temp: ${Math.round(
@@ -63,14 +52,65 @@ function displayData(data) {
   )}&deg <br> <img src='http://openweathermap.org/img/wn/${
     data[0].weather[0].icon
   }@4x.png'></p></div>`;
-  //build out table for 7 day forecast
-  newHTML += `<div><table class="weather-table">`;
+  newHTML += tableMaker(data);
+  info.innerHTML = newHTML;
+  
+  return data;
+}
+
+//function that converts temperatures to celsius
+function displayDataCelsius(data) {
+  const temp = document.querySelectorAll(
+    ".temp, .temp-min, .temp-max, .temp-feels-like"
+  );
+  for (i = 0; i < temp.length; i++) {
+    if (temp[i].className === "temp") {
+      temp[i].innerHTML = `<p>${data[0].main.temp}&deg</p>`;
+    }
+    if (temp[i].className === "temp-min") {
+      for (j = 0; j < data[1].daily.length; j++) {
+        temp[i].innerHTML = `<th>${data[1].daily[j].temp.min}&deg</th>`;
+      }
+    }
+    if (temp[i].className === "temp-max") {
+      for (j = 0; j < data[1].daily.length; j++) {
+        temp[i].innerHTML = `<th>${data[1].daily[j].temp.max}&deg</th>`;
+      }
+    }
+    if (temp[i].className === "temp-feels-like") {
+      for (j = 0; j < data[1].daily.length; j++) {
+        temp[i].innerHTML = `<th>${data[1].daily[j].feels_like.day}&deg</th>`;
+      }
+    }
+  }
+}
+
+//generate current date
+function getDate(data) {
+  const date = new Date(data[0].dt * 1000);
+  const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  newHTML = `<div><h4 class="date">${date.toLocaleDateString(
+    "en-US",
+    options
+  )}</h4>`;
+
+  return newHTML;
+}
+
+//function to construct table using html elements
+function tableMaker(data) {
+  let newHTML = `<div><table class="weather-table">`;
   //headers
   newHTML += `<thead>
         <tr>
           <th class='outside-blocks'><b>Day</b></th><th class='outside-blocks'><b>Min</b></th><th class='outside-blocks'><b>Max</b></th><th class='outside-blocks'><b>Will Feel Like</b></th><th class='outside-blocks'><b>Weather</b></th>
         </tr>`;
-  //write a loop to write information for all 7 days of weather in table
+  //write a loop to write information for all 7 days of weather in table (degrees in farenheit)
   for (i = 1; i < data[1].daily.length; i++) {
     newHTML += `<tr>
           <th class='outside-blocks'>${i}</th><th class='temp-min'>${Math.round(
@@ -85,9 +125,8 @@ function displayData(data) {
        </tr>`;
   }
   newHTML += `</table></div>`;
-  info.innerHTML = newHTML;
-  //return the data for use by tempChangeButton function
-  return data;
+
+  return newHTML;
 }
 
 //button that allows the user to switch between farenheit and celsius temperatures
@@ -98,38 +137,19 @@ function tempChangeButton(data) {
   tempButton.className = "change-temp";
   searchBox.appendChild(tempButton);
   tempButton.addEventListener("click", (e) => {
-    e.target.innerHTML = "Change to Farenheit";
-    const temp = document.querySelectorAll(
-      ".temp, .temp-min, .temp-max, .temp-feels-like"
-    );
-    console.log(temp.length);
-    for (i = 0; i < temp.length; i++) {
-      if (temp[i].className === "temp") {
-        temp[i].innerHTML = `<p>${data[0].main.temp}&deg</p>`;
-      }
-      if (temp[i].className === "temp-min") {
-        for (j = 0; j < data[1].daily.length; j++) {
-          temp[i].innerHTML = `<th>${data[1].daily[j].temp.min}&deg</th>`;
-        }
-        
-      }
-      if (temp[i].className === "temp-max") {
-        for (j = 0; j < data[1].daily.length; j++) {
-          temp[i].innerHTML = `<th>${data[1].daily[j].temp.max}&deg</th>`;
-        }
-      }
-      if (temp[i].className === "temp-feels-like") {
-        for (j = 0; j < data[1].daily.length; j++) {
-          temp[i].innerHTML = `<th>${data[1].daily[j].feels_like.day}&deg</th>`;
-        }
-      }
+    if (e.target.innerHTML === "Change to Celsius") {
+      displayDataCelsius(data);
+      e.target.innerHTML = "Change to Farenheit";
+    } else {
+      displayDataFarenheit(data);
+      e.target.innerHTML = "Change to Celsius";
     }
   });
 }
 
 //error handler
 function notFound(err) {
-  newHTML = `<p>City not found. Please try again<p>`;
+  let newHTML = `<p>City not found. Please try again<p>`;
   document.getElementById("info").innerHTML = newHTML;
   console.log(err);
 }
